afdb7e548ad18ae8238ecfafa7c5fb44eaa38881
Jacob Williams <jacobwilliamskip22@gmail.com>
2023-10-05 19:22:08 -0400

    Add new route for about page
    This added about.css about.html about.js. Also, added the route to app.js.
    Finally, how-to-use.html was added to be worked on.

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const port = 3000;
 const indexRoute = require('./routes/index');
 const homeRoute = require('./routes/home');
 const accountRoute = require('./routes/account');
+const aboutRoute = require('./routes/about');
 
 
 app.use('/', indexRoute);
 app.use('/home', homeRoute);
 app.use('/account', accountRoute);
+app.use('/about-us', aboutRoute);
 app.use(express.static('./images'));
 app.use(express.static('styles'));
 
