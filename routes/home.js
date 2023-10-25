1bd0d3e6c2dddbf4c4622347b2180709b07ef336
Jacob Williams <jacobwilliamskip22@gmail.com>
2023-10-24 21:20:23 -0400

    Fixed bugs with naming and the server side file

--- routes/home.js
+++ routes/home.js
@@ -12,16 +12,16 @@ router.get('/', (req, res) => {
 
 
 // JavaScript code for handling card click
-document.addEventListener('DOMContentLoaded', (event) => {
-    // ... Your existing JavaScript code ...
+// document.addEventListener('DOMContentLoaded', (event) => {
+//  ... Your existing JavaScript code ...
 
-    // Handle the image click to toggle text
-    document.querySelectorAll('.card img').forEach((img) => {
-        img.addEventListener('click', (event) => {
-            const card = event.target.closest('.card');
-            card.classList.toggle('active');
-        });
-    });
-});
+// Handle the image click to toggle text
+//     document.querySelectorAll('.card img').forEach((img) => {
+//         img.addEventListener('click', (event) => {
+//             const card = event.target.closest('.card');
+//             card.classList.toggle('active');
+//         });
+//     });
+// });
 
 module.exports = router;
