45f6bfd51d046a1ff62db072f5f0df1c4709857f
vickietee <vhumberger@gmail.com>
2023-10-27 04:06:43 -0400

    Use session to store user's email on successful login

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,37 @@
+require('dotenv').config();
+
 const express = require('express');
+const sessions = require('express-session');
+const app = express();
 const router = express.Router();
 const path = require('path');
-var app = express()
 const DBHelper = require("../util/DBHelper");
+const oneDay = 1000 * 60 * 60 * 24;
 
+app.use(sessions({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+  }));
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
 });
 
 router.post('/submit-login', (req, res) => {
-    email = req.body.Email
-    password = req.body.Password
+    email = req.body.Email;
+    password = req.body.Password;
  
     DBHelper.QueryDB("Login", "Users", {Email: email}, (results) => {
         // DISPLAY THAT USERNAME OR PASSWORD IS INCORRECT
-        if(results.length == 0 || results[0].Password != password){
-            res.send('LOGIN_FAILED')
-        }
-        else{
-            res.send('LOGIN_SUCCESS')
+        if (results.length == 0 || results[0].Password != password) {
+            res.send('LOGIN_FAILED');
+        } else {
+            req.session.userEmail = email;
+            res.send('LOGIN_SUCCESS');
         }
     });
 });
-
 
 module.exports = router
