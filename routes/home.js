c9ed65240dffe8d58239b4baeee23b486e795f82
vickietee <vhumberger@gmail.com>
2023-10-27 04:07:05 -0400

    Redirect to login if user email session not set

--- routes/home.js
+++ routes/home.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const path = require('path');
 const DBHelper = require("../util/DBHelper");
 
-
-// TODO: REDIRECT TO LOGIN PAGE IF USER IS NOT LOGGED IN
-// POSSIBLE SOLUTION: VALID USER AND PASS MUST BE PASSED TO PAGE IN ORDER FOR IT TO LOAD.
 router.get('/', (req, res) => {
+    if (!req.session.userEmail) {
+        res.redirect('/');
+    }
+    console.log(req.session.userEmail);
     res.sendFile(path.join(__dirname, '../', 'views', 'home.html'));
 });
-
 
 // JavaScript code for handling card click
 // document.addEventListener('DOMContentLoaded', (event) => {
